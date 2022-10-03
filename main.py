2b3edad7efb884ba5567497c7401bb2a105913dd
pedroLarrea <pedroivanlarrea@gmail.com>
2022-10-03 14:21:33 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from mazo import Mazo
 from carta import Carta
 from jugador import Jugador
 
-maso = Mazo()
+mazo = Mazo()
 
-bot = Jugador([maso.obtenerCarta(), maso.obtenerCarta()], maso.obtenerMazo())
-bot.calcularCombinacion([maso.obtenerCarta(), 
-                        maso.obtenerCarta(),
-                        maso.obtenerCarta(),
-                        maso.obtenerCarta(),
-                        maso.obtenerCarta()])
+mazoOriginal = list(mazo.obtenerMazo())
+
+bot = Jugador([mazo.obtenerCarta(), mazo.obtenerCarta()], mazoOriginal)
+# bot.verMano()
+bot.calcularCombinacion([mazo.obtenerCarta(), mazo.obtenerCarta(), mazo.obtenerCarta()])
+# bot.manoActual()
